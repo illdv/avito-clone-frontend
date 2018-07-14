7e7db8a4cc20d6119795e96a1263f191718253c3
Bozhkov Konstantin <bozhkov.k@cronix.ms>
2018-07-14 11:40:18 +0300

    feat(*): Addition dynamic tags to header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,10 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+    static async getInitialProps (ctx) {
+        return await Document.getInitialProps(ctx)
+    }
+
     render() {
         return (
             <html>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head'
 
-import { withI18next } from '../common/lib/withI18next'
+import { withI18next } from '../common/lib/withI18next';
 
-export default withI18next(['home', 'common'])(({ t, initialI18nStore }) => (
-	<div>
-		<h1>{t('welcome')}</h1>
-		<p>{t('common:integrates_react-i18next')}</p>
-		<Link href='/page2'>
-			<a>{t('link.gotoPage2')}</a>
-		</Link>
-	</div>
-))
+interface IIndexProps {
+	t: any;
+}
+
+export class Index extends React.Component<IIndexProps> {
+	static async getInitialProps({ qyery }){
+		return {title: 'Suka'}
+	}
+	render(){
+		const { t } = this.props;
+		return (
+			<React.Fragment>
+				<Head>
+					<meta property="og:description" content='Content' />
+					<title>Index page</title>
+				</Head>
+				<h1>{t('welcome')}</h1>
+				<p>{t('common:integrates_react-i18next')}</p>
+				<Link href='/page2'>
+					<a>{t('link.gotoPage2')}</a>
+				</Link>
+			</React.Fragment>
+		)
+	}
+}
+
+export default withI18next(['home', 'common'])(Index);
