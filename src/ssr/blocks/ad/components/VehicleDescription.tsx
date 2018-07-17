06d33f787db7eedcaa1efaf76db036e9d00252fe
Maxim Zybin <zybin.m@cronix.ms>
2018-07-17 16:36:32 +0300

    aDD SHOW

---
+++ src/ssr/blocks/ad/components/VehicleDescription.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export interface IDescription {
+    body: string,
+}
+
+class DescriptionAd extends Component<IDescription> {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12 col-lg-10 ">
+                    <h3 className="caption p-b-30">
+                        Vehicle Description
+                    </h3>
+                    <span>
+                        {this.props.body}
+                </span>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default DescriptionAd;
