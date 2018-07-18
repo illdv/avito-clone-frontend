67b1a47797fe8e361eeba8ea15ce3c2bc3d585ff
Nikita Kamyshenko <kamyshenko.n@cronix.ms>
2018-07-18 14:20:45 +0300

    feat(Router) fix

--- server/router/config.ts
+++ server/router/config.ts
@@ -1,20 +1,24 @@
 export interface IRoute {
-    path: string;
-    page: string;
-    prepare: string[];
+	path: string;
+	page: string;
+	prepare: string[];
 }
 
 const routes = [
-    {
-        path: '/',
-        page: '/index',
-        prepare: [ 'ads' ],
-    },
-    {
-        path: '/ad/:id',
-        page: '/ad',
-        prepare: [ 'ads' ],
-    },
+	{
+		path: '/',
+		page: '/index',
+		prepare: ['ads'],
+	},
+	{
+		path: '/ad/:id',
+		page: '/ad',
+		prepare: ['ad'],
+	},
+	{
+		path: '/profile',
+		page: '/profile',
+	},
 ] as IRoute[];
 
 export const getRoutes = () => routes;
