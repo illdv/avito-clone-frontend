23014e662f1f82b722ec7db46d10b2993d5d1e05
Nikita Kamyshenko <kamyshenko.n@cronix.ms>
2018-07-18 18:58:53 +0300

    feat(Profile) show user name and href for /profile

--- client/common/store/storeInterface.ts
+++ client/common/store/storeInterface.ts
@@ -1,5 +1,7 @@
 import { IModalJugglerState } from '../modal-juggler/modalJugglerInterface';
+import user from 'client/common/user/reducer';
 
 export interface IRootState {
-    modalJuggler: IModalJugglerState;
+	modalJuggler: IModalJugglerState;
+	user: IUser;
 }
--- client/common/user/saga.tsx
+++ client/common/user/saga.tsx
@@ -6,48 +6,51 @@ import { UserActions } from 'client/common/user/actions';
 import { UserAPI } from 'api/UserAPI';
 import { errorHandler } from 'client/common/store/errorHandler';
 import { CustomStorage } from 'client/common/user/CustomStorage';
+import { hideLoginModal } from 'client/ssr/modals/auth/loginModalTriggers';
 
 function* saveTokenInStore(action: Action<IUser>) {
-  const token = action.payload.token;
-  CustomStorage.setToken(token);
-  axios.defaults.headers.common.authorization = `Bearer ${token}`;
+	const token = action.payload.token;
+	CustomStorage.setToken(token);
+	axios.defaults.headers.common.authorization = `Bearer ${token}`;
 }
 
 function* clearToken() {
-  CustomStorage.clear();
-  axios.defaults.headers.common.authorization = ``;
+	CustomStorage.clear();
+	axios.defaults.headers.common.authorization = ``;
 }
 
 function* login(action: Action<ILoginRequest>) {
-  try {
-    const response: AxiosResponse<{ token: string }> = yield call(UserAPI.login, action.payload);
-    yield put(UserActions.login.SUCCESS({
-      email: action.payload.email,
-      token: response.data.token,
-    }));
-  } catch (e) {
-    yield call(errorHandler, e);
-    yield put(UserActions.login.FAILURE({}));
-  }
+	try {
+		const response: AxiosResponse<{ token: string }> = yield call(UserAPI.login, action.payload);
+		yield put(UserActions.login.SUCCESS({
+			email: action.payload.email,
+			token: response.data.token,
+		}));
+		hideLoginModal();
+	} catch (e) {
+		yield call(errorHandler, e);
+		yield put(UserActions.login.FAILURE({}));
+	}
 }
 
 function* register(action: Action<IRegisterRequest>) {
-  try {
-    yield call(UserAPI.register, action.payload);
-    yield put(UserActions.register.SUCCESS({}));
-  } catch (e) {
-    yield call(errorHandler, e);
-    yield put(UserActions.register.FAILURE({}));
-  }
+	try {
+		yield call(UserAPI.register, action.payload);
+		yield put(UserActions.register.SUCCESS({}));
+		hideLoginModal();
+	} catch (e) {
+		yield call(errorHandler, e);
+		yield put(UserActions.register.FAILURE({}));
+	}
 }
 
 function* watcherUser() {
-  yield [
-    takeEvery(UserActions.login.REQUEST, login),
-    takeEvery(UserActions.login.SUCCESS, saveTokenInStore),
-    takeEvery(UserActions.register.REQUEST, register),
-    takeEvery(UserActions.logout.REQUEST, clearToken),
-  ];
+	yield [
+		takeEvery(UserActions.login.REQUEST, login),
+		takeEvery(UserActions.login.SUCCESS, saveTokenInStore),
+		takeEvery(UserActions.register.REQUEST, register),
+		takeEvery(UserActions.logout.REQUEST, clearToken),
+	];
 }
 
 export default [watcherUser];
