34f7b68d5b3d6889a214d65c363a834338099a46
Nikita Kamyshenko <kamyshenko.n@cronix.ms>
2018-07-20 16:32:04 +0300

    feat(MyAd) add button Activate, Complete

--- client/spa/pages/MyAds.tsx
+++ client/spa/pages/MyAds.tsx
@@ -25,82 +25,6 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
 	adsActions: bindModuleAction(AdsActions, dispatch),
 });
 
-const Item = (props: { ad: IAds, onRemove: () => void, onChangeStatus: () => void }) => {
-	const { ad, onRemove, onChangeStatus } = props;
-	const { title, id, price }             = ad;
-
-	return (
-		<div
-			key={id}
-			className='offer-block__item'
-		>
-			<input type='checkbox' />
-			<div className='offer-block__inner'>
-				<div className='row no-gutters'>
-					<div className='col-md-3 col-lg-3'>
-						<img
-							src='/static/img/ads/ads3.png'
-							alt=''
-							className='offer-block__img'
-						/>
-					</div>
-					<div className='col-md-6 col-lg-6'>
-						<a
-							href='#'
-							className='f-s-16'
-						>
-							<h5>
-								{title}
-							</h5>
-						</a>
-						<span className='d-inline-block offer-block__price'>{price}</span>
-						<div className='publish-offer'>
-							<a
-								className='btn button button_dark-outline publish-offer__button'
-								onClick={onChangeStatus}
-							>
-								{ad.is_active === 1 ? 'Deactivation' : 'Activate'}
-							</a>
-							<a
-								onClick={onRemove}
-								className='btn button button_dark-outline publish-offer__button'
-							>
-								Remove
-							</a>
-						</div>
-					</div>
-					<div className='col-md-3 col-lg-3 text-right edit-block'>
-						<a
-							href='#'
-							className='edit-block__link'
-						>
-							Edit
-						</a>
-						<div className='watcher'>
-							<i className='watcher__icon fa fa-eye' /> <span>---</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
-
-const FilterButton = (props: { isActive, name: string, cont: number, onClick: () => void }) => {
-	const { cont, onClick, isActive, name } = props;
-	return (
-		<div className={`filter-offer__item ${isActive ? 'link-active' : ''}`}>
-			<a
-				onClick={onClick}
-				className='filter-offer__link'
-			>
-				{name}
-			</a>
-			<span className='text_count-category'> {cont}</span>
-		</div>
-	);
-};
-
 class MyAds extends Component<IProps, IState> {
 
 	state: IState = {
@@ -119,6 +43,113 @@ class MyAds extends Component<IProps, IState> {
 		this.props.adsActions.changeStatus.REQUEST({ status, id });
 	}
 
+	onChangeActive = (id: string) => () => {
+		this.props.adsActions.changeStatus.REQUEST({ status: MyAdsStatus.Active, id });
+		this.props.adsActions.changeStatus.REQUEST({ status: MyAdsStatus.Disapproved, id });
+	}
+
+	ActiveButton = ({ id }: { id: string }) => {
+		const { selectedFilter } = this.state;
+		if (selectedFilter === MyAdsStatus.Disapproved) {
+			return (
+				<a
+					className='btn button button_dark-outline publish-offer__button'
+					onClick={this.onChangeActive(id)}
+				>
+					Activate
+				</a>
+			);
+		}
+
+		if (selectedFilter === MyAdsStatus.Active) {
+			return (
+				<a
+					className='btn button button_dark-outline publish-offer__button'
+					onClick={this.onChangeStatus(MyAdsStatus.Completed, id)}
+				>
+					Complete
+				</a>
+			);
+		}
+
+		return <div />;
+	}
+
+	Item = (props: { ad: IAds, onRemove: () => void }) => {
+		const { ad, onRemove }     = props;
+		const { title, id, price } = ad;
+
+		return (
+			<div
+				key={id}
+				className='offer-block__item'
+			>
+				<input type='checkbox' />
+				<div className='offer-block__inner'>
+					<div className='row no-gutters'>
+						<div className='col-md-3 col-lg-3'>
+							<img
+								src='/static/img/ads/ads3.png'
+								alt=''
+								className='offer-block__img'
+							/>
+						</div>
+						<div className='col-md-6 col-lg-6'>
+							<a
+								href='#'
+								className='f-s-16'
+							>
+								<h5>
+									{title}
+								</h5>
+							</a>
+							<span className='d-inline-block offer-block__price'>{price}</span>
+							<div className='publish-offer'>
+								<this.ActiveButton id={ad.id} />
+								<a
+									onClick={onRemove}
+									className='btn button button_dark-outline publish-offer__button'
+								>
+									Remove
+								</a>
+							</div>
+						</div>
+						<div className='col-md-3 col-lg-3 text-right edit-block'>
+							<a
+								href='#'
+								className='edit-block__link'
+							>
+								Edit
+							</a>
+							<div className='watcher'>
+								<i className='watcher__icon fa fa-eye' /> <span>---</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	FilterButton = (props: { buttonFilter: MyAdsStatus, ads: IAds[] }) => {
+		const { buttonFilter, ads } = props;
+
+		const cont     = filterMyAds(buttonFilter, ads).length;
+		const isActive = buttonFilter === this.state.selectedFilter;
+
+		return (
+			<div className={`filter-offer__item ${isActive ? 'link-active' : ''}`}>
+				<a
+					onClick={this.onSelectFilter(buttonFilter)}
+					className='filter-offer__link'
+				>
+					{buttonFilter}
+				</a>
+				<span className='text_count-category'> {cont}</span>
+			</div>
+		);
+	}
+
 	componentDidMount(): void {
 		this.props.adsActions.getMy.REQUEST({});
 	}
@@ -133,30 +164,20 @@ class MyAds extends Component<IProps, IState> {
 		const selectedFilter = this.state.selectedFilter;
 		const filteredAds    = filterMyAds(selectedFilter, ads);
 
-		const countActive      = filterMyAds(MyAdsStatus.Active, ads).length;
-		const countDisapproved = filterMyAds(MyAdsStatus.Disapproved, ads).length;
-		const countCompleted   = filterMyAds(MyAdsStatus.Completed, ads).length;
-
 		return (
 			<>
 				<div className='filter-offer d-flex'>
-					<FilterButton
-						cont={countDisapproved}
-						name={'Disapproved'}
-						isActive={selectedFilter === MyAdsStatus.Disapproved}
-						onClick={this.onSelectFilter(MyAdsStatus.Disapproved)}
+					<this.FilterButton
+						buttonFilter={MyAdsStatus.Disapproved}
+						ads={ads}
 					/>
-					<FilterButton
-						cont={countActive}
-						name={'Active'}
-						isActive={selectedFilter === MyAdsStatus.Active}
-						onClick={this.onSelectFilter(MyAdsStatus.Active)}
+					<this.FilterButton
+						buttonFilter={MyAdsStatus.Active}
+						ads={ads}
 					/>
-					<FilterButton
-						cont={countCompleted}
-						name={'Completed'}
-						isActive={selectedFilter === MyAdsStatus.Completed}
-						onClick={this.onSelectFilter(MyAdsStatus.Completed)}
+					<this.FilterButton
+						buttonFilter={MyAdsStatus.Completed}
+						ads={ads}
 					/>
 				</div>
 				<div className='remove-offer'>
@@ -171,11 +192,10 @@ class MyAds extends Component<IProps, IState> {
 				<div className='offer-block'>
 					{
 						filteredAds.map(ad => (
-							<Item
+							<this.Item
 								key={ad.id}
 								ad={ad}
 								onRemove={this.onRemove(ad.id)}
-								onChangeStatus={this.onChangeStatus(MyAdsStatus.Active, ad.id)}
 							/>
 						))
 					}
