1cde1feffc335170398a2ee762c31b33210edcae
Bozhkov Konstantin <bozhkov.k@cronix.ms>
2018-07-20 20:17:59 +0300

    fix

--- server/router/prepares.ts
+++ server/router/prepares.ts
@@ -6,8 +6,8 @@ type prepareMethod = (params: any, query: any, path: string) => any;
 const instance = axios.create({
 	baseURL: process.env.API_URL,
 	headers: {
-		'Content-Type': 'application/json',
 		'Accept': 'application/json',
+		'Accept-Language': 'en-US,en;q=0.9',
 	},
 });
 
