ec9acc362b614e613372247e26d88ff936a0d354
Dmitry Uskov <uskov.d@cronix.ms>
2018-08-08 18:20:47 +0300

    fix tests

---
+++ client/common/entities/user/modules/notifications/reduser.test.ts
@@ -0,0 +1,21 @@
+import reducer from './reducer';
+import { notificationActions } from './actions';
+
+const token = 1;
+const initialState = {
+	items: [],
+	noReadCount: 0,
+};
+
+describe('SET_NO_READ_COUNT_REQUEST', () => {
+	it('initial state', () => {
+		expect(reducer(undefined, undefined))
+			.toEqual(initialState);
+	});
+
+	it('SET_NO_READ_COUNT_REQUEST', () => {
+		const action = notificationActions.setNoReadCount.REQUEST( token );
+		expect(reducer(initialState, action))
+			.toEqual({...initialState, noReadCount: token});
+	});
+});
