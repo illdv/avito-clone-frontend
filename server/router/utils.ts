4bd7ecfa28e4e6cb7e4b99eccf76a70fca5ebecf
Nikita Kamyshenko <kamyshenko.n@cronix.ms>
2018-08-20 10:37:20 +0300

    fix(Search) delete eval

--- server/router/utils.ts
+++ server/router/utils.ts
@@ -22,13 +22,13 @@ export function queryStringifyPlus(data) {
 }
 
 export const  getQueryWithLocation = req => {
-	const query = {};
+	const query: any = {};
 	if (req.cookies) {
-		if (eval(req.cookies.idCity)) {
+		if (req.cookies.idCity) {
 			query.city_id = req.cookies.idCity;
-		} else if (eval(req.cookies.idRegion)) {
+		} else if (req.cookies.idRegion) {
 			query.region_id = req.cookies.idRegion;
-		} else if (eval(req.cookies.idCountry)) {
+		} else if (req.cookies.idCountry) {
 			query.country_id = req.cookies.idCountry;
 		}
 	}
