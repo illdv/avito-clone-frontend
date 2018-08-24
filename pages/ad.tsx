4cf23983e8f7dc5c231b2d2f28e8700a748d4a3c
Nikita Kamyshenko <kamyshenko.n@cronix.ms>
2018-08-24 10:56:31 +0300

    fix(Search) bloc total location

--- pages/ad.tsx
+++ pages/ad.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import 'isomorphic-fetch';
 
 import { withI18next } from '../common/lib/withI18next';
 
 import Header from 'client/ssr/blocks/header/Header';
 import Navbar from 'client/ssr/blocks/navbar/Navbar';
-import Search from 'client/ssr/blocks/search/Search';
 import Ad from 'client/ssr/blocks/ad/Ad';
 import SellerModal from 'client/ssr/modals/seller/SellerModal';
 import Footer from 'client/ssr/blocks/footer/Footer';
-import 'isomorphic-fetch';
 import { types } from 'redux-act';
 import { IAd } from 'client/ssr/blocks/ad/interface';
 import { SetCategories } from 'client/ssr/blocks/categories/context';
